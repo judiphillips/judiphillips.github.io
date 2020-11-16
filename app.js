40d8bcf01e20cb20440f0344ede5ea797ce55b1c
Judi Phillips <judi.phillips@me.com>
2020-11-16 14:46:07 -0500

    correct bug in html regarding tabs pane- add heroku newsletter app

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 //jshint esversion:6
 const address = "https://judi.dev/";
+const newsletterAddress = "https://dudley-worms-31227.herokuapp.com/";
 
 document.querySelector("#projects").addEventListener("click", (e) => {
-  let id = e.target.parentElement.id;
-  window.open(`${address}${id}/`);
+  if (e.target.parentElement.id === "newsletter") {
+    window.open(newsletterAddress);
+  } else {
+    let id = e.target.parentElement.id;
+    window.open(`${address}${id}/`);
+  }
 });
