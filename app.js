5725cb8e3aba06ab54c486988517e6d192a6acac
Judi Phillips <judi.phillips@me.com>
2020-11-18 19:27:12 -0500

    update links for apps, update photos

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 //jshint esversion:6
 const address = "https://judi.dev/";
 const newsletterAddress = "https://dudley-worms-31227.herokuapp.com/";
+const forkifyAddress = "https://forkify-jp.netlify.app/";
+const keeperAddress = "https://keeper-jp.netlify.app";
 
 document.querySelector("#projects").addEventListener("click", (e) => {
-  if (e.target.parentElement.id === "newsletter") {
+  let id = e.target.parentElement.id;
+  if (id === "newsletter") {
     window.open(newsletterAddress);
+  } else if (id === "forkify") {
+    window.open(forkifyAddress);
+  } else if (id === "keeper") {
+    window.open(keeperAddress);
   } else {
-    let id = e.target.parentElement.id;
     window.open(`${address}${id}/`);
   }
 });
